b3cc6c49ca608d640eab5a4187a2913f4e53e427
O Adegun <sey.adegun@gmail.com>
2024-12-09 11:12:16 +0000

    changed another contract name

--- backend/contracts/ZettoDex.sol
+++ backend/contracts/ZettoDex.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.27;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-contract Dex {
+contract ZettoDex {
   mapping(address => mapping(address => uint256)) public liquidityPools;
 
   function swap(address tokenIn, address tokenOut, uint256 amountIn) public {}
