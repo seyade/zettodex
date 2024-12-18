5043624180e55f25fe4de614bfa45e887bc89101
O Adegun <sey.adegun@gmail.com>
2024-12-18 17:11:30 +0000

    added swap input component and its initial test

--- frontend/src/app/dex/page.tsx
+++ frontend/src/app/dex/page.tsx
@@ -3,6 +3,7 @@
 import React, { useEffect, useState } from "react";
 import Image from "next/image";
 import { ChevronUp, ChevronDown, ChevronsUpDown } from "lucide-react";
+import SwapInput from "./SwapInput";
 
 type DexProps = {};
 
@@ -13,8 +14,6 @@ function Dex({}: DexProps) {
   const [buyAmount, setBuyAmount] = useState("");
   const [totalSellAmount, setTotalSellAmount] = useState(0);
   const [totalBuyAmount, setTotalBuyAmount] = useState(0);
-  const [exchangeRate, setExchangeRate] = useState(0);
-  // const [inputValue, setInputValue] = useState("");
 
   const onHandleClick = () => {
     alert("Crypto List popup");
@@ -29,119 +28,52 @@ function Dex({}: DexProps) {
     setTotalBuyAmount(Number(value) * 250);
   };
 
-  const handleBuyAmountChange = (
-    event: React.ChangeEvent<HTMLInputElement>
-  ) => {
-    const { value } = event?.currentTarget;
-    setBuyAmount(event?.currentTarget.value);
-    setTotalBuyAmount(Number(value) * 250);
-  };
-
-  const calculateExchangeRate = () => {
-    setExchangeRate(Number(sellAmount) * 215);
-  };
-
-  useEffect(() => {}, []);
-
   return (
     <div>
       <article>
-        <div className="w-1/3 py-6 px-2 bg-[#370617]/50 rounded-xl">
-          <h3 className="font-bold text-lg py-2 px-4 mb-2 bg-slate-900 rounded-full inline-flex">
-            Swap
-          </h3>
-          <form>
-            <div className="SellAmount relative w-full flex bg-[#000814] rounded-xl border-transparent border p-4 min-h-10">
-              <div className="w-full">
-                <span className="text-sm text-white/50 font-semibold">
-                  Sell
-                </span>
-                <input
-                  className="w-full bg-[#000814] placeholder:text-white/75 text-xl font-semibold"
-                  inputMode="decimal"
-                  type="text"
-                  placeholder="0.00"
-                  name="sellAmount"
-                  value={sellAmount}
-                  onChange={handleSellAmountChange}
-                />
-                <span className="flex text-xs text-white/50 font-normal">
-                  ${totalSellAmount ? totalSellAmount : "0.00"}
-                </span>
-              </div>
-              <div className="flex items-center justify-end w-full">
-                <button
-                  className="flex items-center justify-between h-10 py-4 px-3 rounded-lg space-x-2 bg-[#001d3d] text-white"
-                  onClick={onHandleClick}
-                >
-                  <span className="">
-                    <Image
-                      className="rounded-full"
-                      src={"/assets/sol.png"}
-                      alt={"crypto icon"}
-                      width={24}
-                      height={24}
-                    />
-                  </span>
-                  <span className="text-sm font-semibold">SOL</span>
-                  <span>
-                    <ChevronDown />
-                  </span>
+        <section className="flex items-center justify-center h-screen bg-teal-950/10">
+          <div className="w-1/3 py-6 px-4 bg-[#370617]/50 rounded-xl">
+            <h3 className="font-bold text-lg py-2 px-4 mb-2 bg-slate-900 rounded-full inline-flex">
+              Swap
+            </h3>
+            <form>
+              <SwapInput
+                amountType="sellAmount"
+                value={sellAmount}
+                totalAmount={totalSellAmount}
+                cryptoIcon="/assets/sol.png"
+                onChange={handleSellAmountChange}
+                onClick={onHandleClick}
+                inputTitle="Sell"
+                showTotal
+              />
+
+              <div className="z-10 flex justify-center items-center relative py-px">
+                <button className="absolute bg-slate-900 p-2 rounded-full border-4 border-zinc-400">
+                  <ChevronsUpDown />
                 </button>
               </div>
-            </div>
 
-            <div className="z-10 flex justify-center items-center relative py-px">
-              <button className="absolute bg-slate-900 p-2 rounded-full border-4 border-zinc-400">
-                <ChevronsUpDown />
-              </button>
-            </div>
+              <SwapInput
+                amountType="buyAmount"
+                value={totalBuyAmount.toString()}
+                totalAmount={totalBuyAmount}
+                cryptoIcon="/assets/usdc.png"
+                onChange={handleSellAmountChange}
+                onClick={onHandleClick}
+                inputTitle="Buy"
+                showTotal
+                disabled
+              />
 
-            <div className="BuyAmount relative w-full flex bg-[#000814] rounded-xl border-transparent border p-4 min-h-10">
-              <div className="w-full">
-                <span className="text-sm text-white/50 font-semibold">Buy</span>
-                <input
-                  className="w-full bg-[#000814] placeholder:text-white/75 text-xl font-semibold"
-                  inputMode="decimal"
-                  type="text"
-                  placeholder="0.00"
-                  name="buyAmount"
-                  value={totalBuyAmount}
-                  disabled
-                />
-                <span className="flex text-xs text-white/50 font-normal">
-                  ${totalBuyAmount ? totalBuyAmount : "0.00"}
-                </span>
-              </div>
-              <div className="flex items-center justify-end w-full">
-                <button
-                  className="flex items-center justify-between h-10 py-4 px-3 rounded-lg space-x-2 bg-[#001d3d] text-white"
-                  onClick={onHandleClick}
-                >
-                  <span className="">
-                    <Image
-                      className="rounded-full"
-                      src={"/assets/usdc.png"}
-                      alt={"crypto icon"}
-                      width={24}
-                      height={24}
-                    />
-                  </span>
-                  <span className="text-sm font-semibold">SOL</span>
-                  <span>
-                    <ChevronDown />
-                  </span>
+              <div>
+                <button className="flex w-full py-4 px-6 mt-4 justify-center items-center rounded-lg bg-[#002855] hover:bg-[#002855]/75 transition-colors duration-300">
+                  <span className="text-lg font-semibold ">Swap</span>
                 </button>
               </div>
-            </div>
-
-            <div>
-              <button className="flex w-full py-4 px-6 mt-4 justify-center items-center rounded-lg bg-[#002855] hover:bg-[#002855]/75 transition-colors duration-300">
-                <span className="text-lg font-semibold ">Swap</span>
-              </button>
-            </div>
-          </form>
-        </div>
+            </form>
+          </div>
+        </section>
       </article>
     </div>
   );
